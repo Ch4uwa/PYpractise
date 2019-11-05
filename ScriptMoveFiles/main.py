b945022280c37f89136550b1ab96f046fb713ce4
MK <mrtn.karlsson@gmail.com>
2019-11-05 09:21:56 +0100

    Added comments MK

--- ScriptMoveFiles/main.py
+++ ScriptMoveFiles/main.py
@@ -8,17 +8,30 @@ import shutil
 
 # Get current user
 user = os.getlogin()
+# Change directory
 os.chdir('/home/' + user + '/Downloads/')
+# Print current working directory
 print(os.getcwd())
 
-try:
-    # list items in directory
+try: # Try to list directory
     for i in os.listdir():
+        # Loop through current working directory
+        # and split name and extention into vars
         f_name, f_ext = os.path.splitext(i)
         if f_ext == '.pdf' or f_ext == '.docx' or f_ext == '.txt':
-            shutil.move(i, '/home/' + user + '/Documents/')
+            # move extention specified to documents folder
+            try:
+                shutil.move(i, '/home/' + user + '/Documents/')
+            except IOError as mtde:
+                print('ERROR when moving documents')
         else:
-            shutil.move(i, 'moved/')
-    
-except OSError:
-    print('Was an error')
+            # move all other extetions found to another place
+            try:
+                shutil.move(i, 'moved/')
+            except IOError:
+                print('ERROR moving not specified files')
+
+except OSError as e:
+    print('OS System call failed')
+finally:
+    print('Finally statement')
